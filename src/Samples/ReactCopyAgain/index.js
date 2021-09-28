67b85aef07449eb41b0c5005faf08d248354ad6b
Svetlin <svetlimladenov@gmail.com>
2021-09-28 18:52:28 +0300

    Render function components

--- src/Samples/ReactCopyAgain/index.js
+++ src/Samples/ReactCopyAgain/index.js
@@ -1,6 +1,9 @@
 const React = {
   createElement(type, props, ...children) {
-    const propsWithChildren = Object.assign(props, { children });
+    const propsWithChildren = {};
+    if (children.length > 0) {
+      Object.assign(propsWithChildren, { children });
+    }
     return {
       type,
       props: propsWithChildren,
@@ -14,12 +17,28 @@ const ReactDOM = {
     container.appendChild(domElement);
   },
   convertReactElementToDomElement(element) {
+    if (typeof element.type === "function") {
+      return this.extractReactElement(element);
+    } else if (typeof element.type === "string") {
+      return this.createDomElement(element);
+    }
+  },
+  extractReactElement(element) {
+    const createdElement = new element.type();
+    return this.convertReactElementToDomElement(createdElement);
+  },
+  createDomElement(element) {
     const domElement = document.createElement(element.type);
     domElement.append(element.props.children);
     return domElement;
   },
 };
 
-const element = React.createElement("div", {}, "hello there", "hi");
+function Button() {
+  return React.createElement("button", {}, "Click me");
+}
 
-ReactDOM.render(element, document.getElementById("root"));
+const element = React.createElement("div", {}, "hello there", "hi");
+const btn = React.createElement(Button, null);
+
+ReactDOM.render(btn, document.getElementById("root"));
